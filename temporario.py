a6da35ea5907bfb54eea386d0e3dcf214100b885
Gustavo Reis <guuh.reis@icloud.com>
2025-02-26 19:52:33 -0300

    Update temporario.py

--- temporario.py
+++ temporario.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.header("Cabeçalho")
 st.toggle("Toggle")
 st.text_area("Enter text")
-st.text_input("Movie title", "Life of Brian")
+st.text_input("")
 st.button("Botão Salvar")
