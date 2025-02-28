b7a750d2cf44885806678626f4a17c131d6c5e4f
Gustavo Reis <guuh.reis@icloud.com>
2025-02-28 19:36:45 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -8,13 +8,13 @@ st.button("Teste")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header("A cat")
+    st.header("Gato")
     st.image("https://static.streamlit.io/examples/cat.jpg")
 
 with col2:
-    st.header("A dog")
+    st.header("Cachorro")
     st.image("https://static.streamlit.io/examples/dog.jpg")
 
 with col3:
-    st.header("An owl")
+    st.header("Coruja")
     st.image("https://static.streamlit.io/examples/owl.jpg")
