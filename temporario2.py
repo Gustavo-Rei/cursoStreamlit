e149702e36acb8eaaacdf6ccdb69e3bba07b02b1
Gustavo Reis <guuh.reis@icloud.com>
2025-02-28 20:01:08 -0300

    Update temporario2.py

--- temporario2.py
+++ temporario2.py
@@ -2,17 +2,18 @@ import pandas as pd
 import streamlit as st
 
 st.title("Formulário de Cadastro de Aluno")
-    
-with st.form("formulario_aluno"):
+
+with st.form("formulario_aluno", clear_on_submit=True):
     nome = st.text_input("Nome Completo")
     cpf = st.text_input("CPF")
     rg = st.text_input("RG")
     ra = st.text_input("RA")
     email = st.text_input("E-mail")
+    telefone = st.text_input("Telefone")
     data_nascimento = st.date_input("Data de Nascimento")
-        
+    
     enviado = st.form_submit_button("Enviar")
-        
+
 if enviado:
     st.success("Dados enviados com sucesso!")
     st.write("### Dados do Aluno")
@@ -24,3 +25,20 @@ if enviado:
     st.write(f"**E-mail:** {email}")
     st.write(f"**Data de Nascimento:** {data_nascimento}")
 
+    # Armazenando os dados do usuário em um dicionário
+    dados_aluno = {
+        "Nome": nome,
+        "CPF": cpf,
+        "RG": rg,
+        "RA": ra,
+        "Telefone": telefone,
+        "E-mail": email,
+        "Data de Nascimento": data_nascimento,
+    }
+    
+    # Convertendo o dicionário para um DataFrame do pandas
+    df_aluno = pd.DataFrame([dados_aluno])
+    
+    # Exibindo o DataFrame
+    st.write("### Dados Armazenados")
+    st.dataframe(df_aluno)
