0241c10e4eed74b6bc2a2dcf53ae98a2f2b1412c
Gustavo Reis <guuh.reis@icloud.com>
2025-06-06 20:27:08 -0300

    Update brasil.py

--- brasil.py
+++ brasil.py
@@ -81,7 +81,7 @@ df.index += 1  # primeira posição = 1
 # exibição da tabela
 st.dataframe(df)
 
-st.subheader("Evolução dos Pontos por Time")
+st.subheader("Distribuição de Pontos por Time")
 
-# Cria o gráfico de linha com os dados de pontos por time
-st.line_chart(df.set_index("Time")["Pontos"])
+# Cria o gráfico de barras com os dados de pontos por time
+st.bar_chart(df.set_index("Time")["Pontos"])
